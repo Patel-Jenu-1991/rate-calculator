1b2d4b405f75d9ad48328cf3c0e106553d57d43b
Jaineshkumar Patel <jenu.16@icloud.com>
2018-07-04 04:17:03 -0500

    fix bugs, tidy code v1.1

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-
 const rateBox = document.querySelector("#rate"),
-      roomRate = document.querySelector("#roomRate"),
-      taxes = document.querySelector("#taxes"),
-      total = document.querySelector("#total"),
-      TAX_RATE = 0.1304;
+  roomRate = document.querySelector("#roomRate"),
+  taxes = document.querySelector("#taxes"),
+  total = document.querySelector("#total"),
+  TAX_RATE = 1.15; // state 6.00% + county 9.00 = 15.00%
 
-rateBox.addEventListener("change", () => {
+rateBox.addEventListener("input", updateResults);
+
+function updateResults() {
   let totalAmt = parseFloat(Math.round(rateBox.value * 100) / 100).toFixed(2),
-      tax = getTaxes(totalAmt),
-      rate = getRate(totalAmt, tax);
+    tax = getTaxes(parseFloat(totalAmt)),
+    rate = getRate(parseFloat(totalAmt));
 
   roomRate.value = rate;
   taxes.value = tax;
   total.value = totalAmt;
-});
-
-function getTaxes(total) {
-  return parseFloat(Math.round(total * TAX_RATE * 100) / 100).toFixed(2);
 }
 
-function getRate(total, taxes) {
-  const rate = parseFloat(Math.round((total - (total * TAX_RATE)) * 100) / 100).toFixed(2);
-  return rate;
+function getTaxes(total) {
+  return (parseFloat(total) - parseFloat(getRate(total))).toFixed(2);
+}
+
+function getRate(total) {
+  return parseFloat(total / TAX_RATE).toFixed(2);
 }
