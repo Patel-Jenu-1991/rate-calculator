a400d1edf907c654fc0612328e3bab519b67191e
Jaineshkumar Patel <jenu.16@icloud.com>
2018-07-04 19:48:55 -0500

    Minor tweaks, refactor javascript, tax note, inline documentation

--- app.js
+++ app.js
@@ -1,25 +1,54 @@
-const rateBox = document.querySelector("#rate"),
-  roomRate = document.querySelector("#roomRate"),
-  taxes = document.querySelector("#taxes"),
-  total = document.querySelector("#total"),
-  TAX_RATE = 1.15; // state 6.00% + county 9.00 = 15.00%
+// Attach event listener to update the resultBox on input with
+// computed room rate, taxes and total
+rateBox.addEventListener("input", () => updateResults());
 
-rateBox.addEventListener("input", updateResults);
+/*
+ * @description Updates the resultBox, event handler for the rate box
+ */
+const updateResults = () => {
+  let dom = getDOM();
+  (totalAmt = parseFloat(Math.round(dom.rateBox.value * 100) / 100).toFixed(2)),
+    (tax = getTaxes(parseFloat(totalAmt))),
+    (rate = getRate(parseFloat(totalAmt)));
 
-function updateResults() {
-  let totalAmt = parseFloat(Math.round(rateBox.value * 100) / 100).toFixed(2),
-    tax = getTaxes(parseFloat(totalAmt)),
-    rate = getRate(parseFloat(totalAmt));
+  dom.roomRate.value = rate;
+  dom.taxes.value = tax;
+  dom.total.value = totalAmt;
+};
 
-  roomRate.value = rate;
-  taxes.value = tax;
-  total.value = totalAmt;
-}
-
-function getTaxes(total) {
+/*
+ * @description Computes the room rate by deducting taxes from the total
+ * @param {number} total
+ * @returns {string} The room rate rounded off to 2 decimal places
+ */
+const getTaxes = total => {
   return (parseFloat(total) - parseFloat(getRate(total))).toFixed(2);
-}
+};
 
-function getRate(total) {
+/*
+ * @description Computes the taxes
+ * @param {number} total
+ * @returns {string} The taxes rounded off to 2 decimal places
+ */
+const getRate = total => {
+  const TAX_RATE = 1.15; // state 6.00% + county 9.00 = 15.00%
   return parseFloat(total / TAX_RATE).toFixed(2);
-}
+};
+
+/*
+ * @description Selects all the DOM elements to work with
+ * @returns {object} An object containing all the DOM elements
+ */
+const getDOM = () => {
+  const rateInput = document.querySelector("#rate"),
+    roomRateInput = document.querySelector("#roomRate"),
+    taxBox = document.querySelector("#taxes"),
+    totalBox = document.querySelector("#total");
+
+  return {
+    rateBox: rateInput,
+    roomRate: roomRateInput,
+    taxes: taxBox,
+    total: totalBox
+  };
+};
